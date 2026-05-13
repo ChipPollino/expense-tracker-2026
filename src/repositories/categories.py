5cf623bfe5313ff4ccfaa36ce28cb10f4b27b55e
Polina Lepeshkina <Lpolina07@gmail.com>
2026-05-13 22:09:30 +0300

    update categories repository

--- src/repositories/categories.py
+++ src/repositories/categories.py
@@ -3,20 +3,26 @@ from src.repositories.base import BaseRepository
 from src.schemas.categories import CategoryUpdate
 
 
-class CategoryRepository(BaseRepository):
+class CategoriesRepository(BaseRepository):
     model = CategoriesOrm
 
     async def get_all_by_user(self, user_id: int):
         return await self.get_all(user_id=user_id)
 
     async def get_by_id_for_user(self, category_id: int, user_id: int):
-        await self.get_one_or_none(id=category_id, user_id=user_id)
+        return await self.get_one_or_none(id=category_id,
+                                          user_id=user_id)
 
     async def get_by_name_for_user(self, category_name: str, user_id: int):
-        await self.get_one_or_none(name=category_name, user_id=user_id)
+        return await self.get_one_or_none(name=category_name,
+                                          user_id=user_id)
 
     async def change_name(self, data: CategoryUpdate, category_id: int, user_id: int) -> None:
-        await self.edit(data=data, is_patch=True, id=category_id, user_id=user_id)
+        await self.edit(data=data,
+                        is_patch=True,
+                        id=category_id,
+                        user_id=user_id)
 
     async def delete_by_id_for_user(self, category_id: int, user_id: int) -> None:
-        await self.delete(id=category_id, user_id=user_id)
+        await self.delete(id=category_id,
+                          user_id=user_id)
